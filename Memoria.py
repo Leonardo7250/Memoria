09af7305823e6788e8794eaa359098b4d18e9ccc
Leonardo7250 <73494910+Leonardo7250@users.noreply.github.com>
2020-10-29 12:06:49 -0600

    Update Memoria.py

--- Memoria.py
+++ Memoria.py
@@ -56,9 +56,9 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 10, y + 7)
+        goto(x+25, y)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], align="center", font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
